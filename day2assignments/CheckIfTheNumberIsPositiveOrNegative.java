c11d242d8c93900c27835f5c419aaccf70023869
Gopinath <gopinath796@gmail.com>
2023-03-30 22:10:08 +0530

    Add files via upload

---
+++ day2assignments/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,17 @@
+package week2.day2assignments;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		int num = 35;
+		if(num>0) {
+			System.out.println("The number is positive");
+		}else if(num<0) {
+			System.out.println("The number is negative");
+		}else if(num==0){
+			System.out.println("The number is neither positive nor negative");
+		}
+
+	}
+
+}
