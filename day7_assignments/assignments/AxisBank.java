7ea6b814a46a3288b457a42e83ef255b91919d68
Gopinath <gopinath796@gmail.com>
2023-04-10 23:35:04 +0530

    Add files via upload

---
+++ day7_assignments/assignments/AxisBank.java
@@ -0,0 +1,15 @@
+package day7.assignments;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+		System.out.println("Bankinfo details are taken here");
+	}
+
+	public static void main(String[] args) {
+		AxisBank axs = new AxisBank();
+		axs.deposit();
+		axs.fixed();
+		axs.saving();
+
+	}
+}
